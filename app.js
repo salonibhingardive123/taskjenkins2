ed89e0080306da289675792d7617d48102a42772
saloni.bhingardive <saloni.bhingardive@coditas.com>
2026-04-28 16:56:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Docker + Jenkins + Azure ---> This is Saloni!');
+    res.send('Hello from Docker + Jenkins + Azure ---> This is Saloni! Test');
 });
 
 app.listen(3000, () => {
